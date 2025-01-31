9719e19ff4b16b332a436d65ce65e37b420685dc
MarekLukk <mareklukk8@gmail.com>
2025-01-31 16:47:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace NameControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma nini");
+            Console.WriteLine("Sisesta oma nimi:");
+            string nimi = Console.ReadLine();
+
+            if (nimi != "")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Tere " + nimi);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR");
+                Console.Beep();
+                Console.Beep();
+                Console.Beep();
+            }
+                    
+        }
+    }
+}
